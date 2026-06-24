512342bfd5ccb191be41e7d0a789fe1307a677d2
gordon132 <donmoenga@gmail.com>
2026-06-24 23:50:47 +0300

    Create medium-13.sql

---
+++ intermediate/medium-13.sql
@@ -0,0 +1,7 @@
+# Display each patient's full name in a single column.Their last_name in all upper letters must appear first, then first_name in all lower case letters.
+# Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
+
+SELECT
+	concat(upper(last_name), ',', lower(first_name)) AS new_name_format
+FROM patients
+order by first_name desc
