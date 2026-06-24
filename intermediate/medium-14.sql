6af9999c883659ae24f039fe61abe94a6699cb89
gordon132 <donmoenga@gmail.com>
2026-06-24 23:56:55 +0300

    Create medium-14.sql

---
+++ intermediate/medium-14.sql
@@ -0,0 +1,7 @@
+# Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
+
+SELECT
+	province_id, SUM(height) AS sum_height
+from patients
+group by province_id 
+having sum_height >= 7000;
