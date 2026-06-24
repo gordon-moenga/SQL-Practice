75fb3a81eb39be165fc3f05d1d60ba7e26c329a3
gordon132 <donmoenga@gmail.com>
2026-06-24 23:32:31 +0300

    Create medium-11.sql

---
+++ intermediate/medium-11.sql
@@ -0,0 +1,9 @@
+# Show all allergies ordered by popularity. Remove NULL values from query.
+
+SELECT
+	allergies,
+    COUNT(*) as total_diagnosis
+from patients
+WHERE allergies is NOT NULL
+GROUP BY allergies
+ORDER BY total_diagnosis DEsc
