41eac55d0db200703d2346d487dc319a71029e45
gordon132 <donmoenga@gmail.com>
2026-06-24 22:46:02 +0300

    Update medium-1.sql

--- intermediate/medium-1.sql
+++ intermediate/medium-1.sql
@@ -1,6 +1,6 @@
 # Show unique birth years from patients and order them by ascending.
 
 SELECT 
-	DISTINCT YEAR(birth_date) as birth_year
+	distinct YEAR(birth_date) as birth_year
 FROM patients
 order by birth_year asc;
