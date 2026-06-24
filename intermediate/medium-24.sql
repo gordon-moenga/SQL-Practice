3d5ed0a6bb2e48aef6286b8de61fb92cfa2cc002
gordon132 <donmoenga@gmail.com>
2026-06-25 01:45:46 +0300

    Create medium-24.sql

---
+++ intermediate/medium-24.sql
@@ -0,0 +1,13 @@
+# Display patient's full name, height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.
+# Convert CM to feet by dividing by 30.48. Convert KG to pounds by multiplying by 2.205.
+
+SELECT
+	concat(first_name, ' ', last_name) AS patient_name,
+	ROUND(height / 30.48, 1) AS height,
+	ROUND(weight * 2.205, 0) AS weight,
+    birth_date,
+CASE
+	WHEN gender = 'M' then 'Male'
+	WHEN gender = 'F' then 'Female'
+	END AS gender_type
+from patients;
