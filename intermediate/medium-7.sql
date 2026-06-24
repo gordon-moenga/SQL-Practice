6f92f278c54dac55ff84ed4852a3255ff146fb65
gordon132 <donmoenga@gmail.com>
2026-06-24 23:02:08 +0300

    Create medium-7.sql

---
+++ intermediate/medium-7.sql
@@ -0,0 +1,9 @@
+# Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'.
+# Show results ordered ascending by allergies then by first_name then by last_name.
+
+SELECT
+	first_name, last_name, allergies
+from patients
+WHERE allergies IN ('Morphine', 'Penicillin')
+order by
+	allergies, first_name, last_name;
