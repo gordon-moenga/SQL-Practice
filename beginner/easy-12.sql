8e7962647c7018e20444e1fc06b35cdefcb8eaa5
gordon132 <donmoenga@gmail.com>
2026-06-24 15:50:38 +0300

    Create easy-12.sql

---
+++ beginner/easy-12.sql
@@ -0,0 +1,6 @@
+# Question
+Show all the columns from admissions where the patient was admitted and discharged on the same day.
+# Solution
+SELECT *
+FROM admissions
+WHERE admission_date = discharge_dat
