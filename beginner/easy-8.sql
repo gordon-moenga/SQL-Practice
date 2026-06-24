2169a775d0502d7d9560a8382be9cb7a6c1bc17e
gordon132 <donmoenga@gmail.com>
2026-06-24 16:04:53 +0300

    Update easy-8.sql

--- beginner/easy-8.sql
+++ beginner/easy-8.sql
@@ -1,4 +1,4 @@
-Show how many patients have a birth_date with 2010 as the birth year.
+# Show how many patients have a birth_date with 2010 as the birth year.
   
 SELECT 
 COUNT(*) as total_patients
