5190056b93d89dd13125d1b879c9b0c920054441
gordon132 <donmoenga@gmail.com>
2026-06-24 22:43:03 +0300

    Create medium-2.sql

---
+++ intermediate/medium-2.sql
@@ -0,0 +1,7 @@
+# Show unique first names from the patients table which only occurs once in the list.
+
+SELECT 
+	DISTINCT(first_name)
+FROM patients
+GROUP BY first_name 
+HAVING Count(first_name) = 1;
