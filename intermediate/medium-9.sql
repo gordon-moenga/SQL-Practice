666b54be6aad9e64c490731dc37720fce00a0f88
gordon132 <donmoenga@gmail.com>
2026-06-24 23:13:12 +0300

    Create medium-9.sql

---
+++ intermediate/medium-9.sql
@@ -0,0 +1,9 @@
+# Show the city and the total number of patients in the city. Order from most to least patients and then by city name ascending.
+
+SELECT
+	city,
+    count(*) AS num_patients
+from patients
+group by city 
+ORDER BY
+	num_patients DESC, city;
