8933d7914ace180f95ef5849a7643a6865b83e6e
gordon132 <donmoenga@gmail.com>
2026-06-28 12:09:28 +0300

    Update medium-27.sql

--- intermediate/medium-27.sql
+++ intermediate/medium-27.sql
@@ -1,14 +1,14 @@
 # Display every patient that has at least one admission and show their most recent admission along with the patient and doctor's full name.
 
-select
-	concat(p.first_name, ' ', p.last_name) as patient_name,
+SELECT
+	CONCAT(p.first_name, ' ', p.last_name) AS patient_name,
     a.admission_date,
-    concat(d.first_name, ' ', d.last_name) as doctor_name
-FROM patients as p 
-	inner join admissions as a on p.patient_id = a.patient_id
-    inner join doctors as d on a.attending_doctor_id = d.doctor_id
+    CONCAT(d.first_name, ' ', d.last_name) AS doctor_name
+FROM patients AS p 
+	INNER JOIN admissions AS a ON p.patient_id = a.patient_id
+    INNER JOIN doctors AS d ON a.attending_doctor_id = d.doctor_id
 WHERE admission_date = (SELECT
-                        max(a.admission_date)
-                        from admissions as a
+                        MAX(a.admission_date)
+                        FROM admissions AS a
                         WHERE a.patient_id = p.patient_id
                         );
